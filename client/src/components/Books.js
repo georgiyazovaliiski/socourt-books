fd51235f5039956757805544511be7c4a119a262
Georgi Yazovaliyski <georgi.yazovaliiski@gmail.com>
2019-09-01 10:17:41 -0400

    Backend Done Front End in Progress

---
+++ client/src/components/Books.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Books = () =>{
+    return (
+        <div className={'container'}>
+            <h1>Working</h1>
+        </div>
+    )
+}
+
+export default Books;
